2d34ec8f44b377f68f65461474cf7227cdb2ca14
surachai subnalsing <143887257+Surachaizxzx@users.noreply.github.com>
2024-10-09 22:43:41 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,14 +8,16 @@ import { Route, Routes, BrowserRouter } from 'react-router-dom';
 export default function app() {
   return (
 
-    <BrowserRouter>
+    <>
       <Navbar />
-      <Routes>
-        <Route path='/' element={<Section />} />
-        <Route path='/ShortUrl' element={<ShortUrl />} />
-        <Route path='/List' element={<List />} />
-      </Routes >
-    </BrowserRouter >
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Section />} />
+          <Route path='/ShortUrl' element={<ShortUrl />} />
+          <Route path='/List' element={<List />} />
+        </Routes >
+      </BrowserRouter >
+    </>
 
 
   )
